2040e97565f6d541533346674c45e331520d2342
DannSant <38538655+DannSant@users.noreply.github.com>
2020-01-24 11:06:06 -0600

    Delete config.ts

--- src/app/config/config.ts
+++
@@ -1,9 +0,0 @@
-//export const SERVICE_URL = 'http://localhost:3000';
-export const SERVICE_URL = 'https://raven-ink.herokuapp.com';
-
-export const ADMIN_ROLE_CODE = '1';
-
-export const PAYU_API_KEY = 'xcZq1hzu4KoR34CWaA3kO3JRi5Api ';
-export const PAYU_MERCHANT_ID = '793038';
-export const PAYU_ACCOUNT_ID = '512324';
-export const PAYU_CURRENCY = 'MXN';
